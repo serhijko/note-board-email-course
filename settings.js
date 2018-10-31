b29feb8a4dc4e7908d96a272633187d79a358980
serhijko <sp_by@tut.by>
2018-10-31 23:30:58 +0300

    initial commit

---
+++ settings.js
@@ -0,0 +1,23 @@
+// Uncomment the following line to load our .env file and add the values to process.env
+// require('dotenv').config({ silent: true });
+
+module.exports = {
+  port: process.env.PORT || 3000,
+  env: process.env.ENV || 'development',
+
+  // Environment-dependent settings
+  development: {
+    db: {
+      dialect: 'sqlite',
+      storage: ':memory:',
+      operatorsAliases: false,
+    },
+  },
+  production: {
+    db: {
+      dialect: 'sqlite',
+      storage: 'db/database.sqlite',
+      operatorsAliases: false,
+    },
+  },
+};
