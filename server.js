c85518ddeef151911c18713843a82f8d078e1e17
serhijko <sp_by@tut.by>
2018-11-12 22:04:13 +0300

    Adding Support for Views on the Server, and Serving Static Files

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
+const Pug = require('pug');
+const Vision = require('vision');
 const Models = require('./lib/models/');
 const Routes = require('./lib/routes');
 const Settings = require('./settings');
 const server = new Hapi.Server({ port: Settings.port });
 
-server.route(Routes);
-
 const init = async () => {
+  await server.register([ Vision, Inert ]);
+
+  server.views({
+    engines: { pug: Pug },
+    path: Path.join(__dirname, 'lib/views'),
+    compileOptions: {
+      pretty: false,
+    },
+    isCached: Settings.env === 'production'
+  });
+
+  server.route(Routes);
+
   await server.start();
   console.log(`Server running at: ${server.info.uri}`);
 };
